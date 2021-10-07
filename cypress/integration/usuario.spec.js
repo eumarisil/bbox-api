59b1583ce5314668d5e58b3ae60aa3cfa66b68a0
Mariana Silva <83925799+maridoqa@users.noreply.github.com>
2021-10-07 14:28:05 -0300

    alteracao

--- cypress/integration/usuario.spec.js
+++ cypress/integration/usuario.spec.js
@@ -17,5 +17,23 @@ describe('Cadastrar uusário', () => {
             expect(Response.status).to.eq(200)
             expect(Response.body.token).to.exist
     })
-});
+}) 
+
+    it('Deve validar email invalido', () => {
+        cy.request({
+            method: 'POST',
+            url: '/v1/users', failOnStatusCode: false,
+            body: {
+                "fullName": faker.name.findName(),
+                "password": "123456",
+                "email": "teste.com",
+                "loginType": "email"
+            }
+        }).should((Response) => {
+            expect(Response.status).to.eq(400)
+            expect(Response.body.error.message).to.eq('O e-mail é inválido')
+        })
+    })
+
+ 
 });
